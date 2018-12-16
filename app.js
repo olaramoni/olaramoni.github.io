d25099641c63da514d52e2baa5c59b4ce56cdb0c
Ola <ola.ramoni@ada.ac.uk>
2018-12-16 13:05:28 +0000

    Change site to U14 work

--- app.js
+++ app.js
@@ -1,108 +1,95 @@
-let userScore = 0;
-let compScore = 0;
-const userScore_span = document.getElementById("user-score");
-const compScore_span = document.getElementById("comp-score");
-const scoreBoard_div = document.querySelector(".scoreboard");
-const result_p = document.querySelector(".result > p");
-const rock_div = document.getElementById("r");
-const paper_div = document.getElementById("p");
-const scissors_div = document.getElementById("s");
-const lizard_div = document.getElementById("l");
-const spock_div = document.getElementById("sp")
+var gravity = 0.3;
+var players =
+    {
+      playerOne:
+      {
+        lives: 5,
+        moveLeft: "a",
+        moveRight: "s",
+        sprite: "",
+        leftBoundary:0,
+        rightBoundary:500,
+        xPosition:250,
+      },
+      playerTwo:
+      {
+        lives: 5,
+        moveRight: "h",
+        moveLeft: "g",
+        sprite: "",
+        leftBoundary:500,
+        rightBoundary:1000,
+        xPosition:750,
+      }
+    }
 
-function getComputerChoice(){
-  const choices = ["r","p","s","l","sp"];
-  const randomNumber = Math.floor(Math.random() * 5);
-  return choices[randomNumber];
-}
-
-function convertToWord(letter){
-  if (letter === "r") return "Rock";
-  if (letter === "p") return "Paper";
-  if (letter === "s") return "Scissors";
-  if (letter === "l") return "Lizard";
-  if (letter === "sp") return "Spock";
-}
-
-function win(user, computer){
-  userScore++;
-  userScore_span.innerHTML = userScore
-  compScore_span.innerHTML = compScore
-  const smallUserWord = "user".fontsize(3).sub();
-  const smallCompWord = "comp".fontsize(3).sub();
-  result_p.innerHTML = `${convertToWord(user)}${smallUserWord} beats ${convertToWord(computer)}${smallCompWord}. You win! 🔥🔥🔥`
-}
-
-function lose(user, computer){
-  compScore++;
-  userScore_span.innerHTML = userScore
-  compScore_span.innerHTML = compScore
-  const smallUserWord = "user".fontsize(3).sub();
-  const smallCompWord = "comp".fontsize(3).sub();
-  result_p.innerHTML = `${convertToWord(user)}${smallUserWord} loses to ${convertToWord(computer)}${smallCompWord}. You lose💩💩💩`
-}
-
-function draw(user, computer){
-  result_p.innerHTML = `You both chose ${convertToWord(user)}. It's a draw!`
-}
-
-function game(userChoice) {
-  const computerChoice=getComputerChoice();
-  switch(userChoice + computerChoice) {
-    case "rs":
-    case "rl":
-    case "pr":
-    case "psp":
-    case "sp":
-    case "sl":
-    case "sps":
-    case "spr":
-    case "lsp":
-    case "lp":
-      win(userChoice, computerChoice);
-      break;
-    case "rp":
-    case "rsp":
-    case "ps":
-    case "pl":
-    case "sr":
-    case "ssp":
-    case "lr":
-    case "ls":
-    case "spl":
-    case "spp":
-      lose(userChoice, computerChoice);
-      break;
-    case "rr":
-    case "pp":
-    case "ss":
-    case "ll":
-    case "spsp":
-      draw(userChoice, computerChoice);
-      break;
+var sprites = {
+  ball:{
+    sprite:""
+  },
+  walls:{
+    rightWall:"",
+    leftWall:"",
+    bottomWall:"",
+    topWall:""
+  },
+  divider:{
+  divOne:""
   }
 }
 
-function main() {
-  rock_div.addEventListener("click", function() {
-    game("r");
-  })
+function setup(){
+    createCanvas(1000,700)
+    players.playerOne.sprite=createSprite(players.playerOne.xPosition, 600, 100,64)
+    players.playerOne.sprite.addAnimation("playerOne", "images/playerOne.png")
+    players.playerTwo.sprite=createSprite(players.playerTwo.xPosition, 600, 100,64)
+    players.playerTwo.sprite.addAnimation("playerTwo", "images/playerTwo.png")
+    sprites.walls.rightWall=createSprite(1000,350,1,700)
+    sprites.walls.rightWall.immovable=true
+    sprites.walls.leftWall=createSprite(0,350,1,700)
+    sprites.walls.leftWall.immovable=true
+    sprites.walls.bottomWall=createSprite(500,700,1000,1)
+    sprites.walls.bottomWall.immovable=true
+    sprites.walls.topWall=createSprite(500,0,1000,1)
+    sprites.walls.topWall.immovable=true
+  }
 
-  paper_div.addEventListener("click", function() {
-    game("p");
-  })
-
-  scissors_div.addEventListener("click", function() {
-    game("s");
-  })
-
-  spock_div.addEventListener("click", function() {
-    game("sp");
-  })
-
-  lizard_div.addEventListener("click", function() {
-    game("l");
-  })
+function draw(){
+  constrain(players.playerOne.sprite.position.x, 0, 500)
+  constrain(players.playerTwo.sprite.position.x, 0, 500)
+  clear()
+  createCanvas(1000,700)
+  background(200,200,200)
+  sprites.ball.sprite.addSpeed(gravity,90)
+  sprites.ball.sprite.bounce(sprites.walls.leftWall)
+  sprites.ball.sprite.bounce(sprites.walls.rightWall)
+  sprites.ball.sprite.bounce(sprites.walls.topWall)
+  sprites.ball.sprite.bounce(sprites.walls.bottomWall)
+  if(keyDown("a")){
+  players.playerOne.sprite.position.x-=4
+  }
+  if(keyDown("d")){
+  players.playerOne.sprite.position.x+=4
+  }
+  if(keyDown("j")){
+  players.playerTwo.sprite.position.x+=4
+  }
+  if(keyDown("l")){
+    players.playerThree.sprite.position.x-=4
+  }
+  if(sprites.ball.sprite.bounce(players.playerOne.sprite)){
+    sprites.ball.sprite.setSpeed(20,random(220,320))
+  }
+  if(sprites.ball.sprite.bounce(players.playerTwo.sprite)){
+    sprites.ball.sprite.setSpeed(20,random(220,320))
+  }
+  if (sprites.ball.sprite.position.y>650){
+    noLives()
+    sprites.ball.sprite.changeAnimation("Collision")
+    sprites.ball.sprite.setSpeed(0,0)
+    sprites.ball.sprite.addSpeed(gravity,270)
+    setTimeout(livesRemover,2960)
+    setTimeout(resetBall,3000)
+  }
+  drawSprites()
 }
-
-main();
